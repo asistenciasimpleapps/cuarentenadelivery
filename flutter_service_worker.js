aba77fde2be65923de892228ae97e61ef6fb355c
asistenciasimpleapps <asistenciasimpleapps@users.noreply.github.com>
2020-05-19 00:37:49 +0000

    deploy: d596802d32c75b0690be546ad95692facdee769f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,7 +1,7 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "2d0ba292e45600cf76fc05c0b93d4cf8",
+  "main.dart.js": "fed5177226d1c32fdeac5097e14c5f59",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/fonts/big_noodle_titling.ttf": "ff78c5e2b6c3846035dae28cdaa65583",
 "assets/fonts/big_noodle_titling_oblique.ttf": "52629e481c4d4df7a9783ed08bdecdef",
